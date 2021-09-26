62de807a323d368c208889a0d26a0828b2a2d9fe
Sahil Suvarna <sahil.suvarna786@gmail.com>
2021-09-26 12:45:32 +0530

    chat update

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatApp2/widgets/chat/messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -34,25 +35,12 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('chats/9h7ZmVW2Ar2Szhm4Ri20/messages')
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final documents = streamSnapshot.data.documents;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[index]['text']),
-            ),
-          );
-        },
+      body: Container(
+        child: Column(children: [
+          Expanded(
+            child: Messages(),
+          ),
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
